2a10a3245713535e27740060814e745780f5305a
Hùng Lê <119946155+noname1711@users.noreply.github.com>
2025-06-13 21:04:47 +0700

    mutex vip pro

--- src/server.c
+++ src/server.c
@@ -86,6 +86,9 @@ void* handle_client(void* arg) {
                 } else if (status == -1) {
                     snprintf(response, sizeof(response), 
                             "Mutex '%s' already locked by another client", cmd.mutex_name);
+                } else if (status == -2) {
+                    snprintf(response, sizeof(response), 
+                            "Mutex '%s' already locked by this client", cmd.mutex_name);
                 } else if (status == -3) {
                     snprintf(response, sizeof(response), "There is already a mutex in the system");
                 } else {
